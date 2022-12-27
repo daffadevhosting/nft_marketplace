6bca646d26e8a0bfbb73913fe48017b6a240b218
daffadevhosting <ndangsaepudin@outlook.com>
2022-12-27 19:41:16 +0700

    update

--- pages/listing/[listingId].jsx
+++ pages/listing/[listingId].jsx
@@ -5,7 +5,22 @@ import {
   useNetworkMismatch,
   useListing,
 } from "@thirdweb-dev/react";
-import { Spinner } from '@chakra-ui/react'
+import {
+  Container,
+  Stack,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Icon,
+  IconButton,
+  createIcon,
+  IconProps,
+  useColorModeValue,
+  Spinner
+} from '@chakra-ui/react';
 import { ChainId, ListingType, NATIVE_TOKENS } from "@thirdweb-dev/sdk";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -13,6 +28,7 @@ import { MARKETPLACE_ADDRESS } from "../../const/contractAddresses";
 import styles from "../../styles/Theme.module.css";
 
 const activeChainId = parseInt(`${process.env.NEXT_PUBLIC_CHAIN_ID}`)
+
 
 export default function ListingPage() {
   const router = useRouter();
@@ -100,75 +116,88 @@ export default function ListingPage() {
       alert(err.message);
     }
   }
-
+  
   return (
-    <div className={styles.container}>
-      <div className={styles.listingContainer}>
-        <div className={styles.leftListing}>
+    <Container maxW={'7xl'}>
+      <Stack
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+        direction={{ base: 'column', md: 'row' }}>
+        <Flex
+          flex={1}
+          justify={'center'}
+          align={'center'}
+          position={'relative'}
+          w={'full'}>
+          <Box
+            position={'relative'}
+            height={'auto'}
+            rounded={'2xl'}
+            boxShadow={'2xl'}
+            width={'full'}
+            overflow={'hidden'}>
           <MediaRenderer
             src={listing.asset.image}
-            className={styles.mainNftImage}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={'100%'}
+			  style={{objectFit: 'cover', objectPosition: 'center'}}
           />
-        </div>
-
-        <div className={styles.rightListing}>
-          <h1>{listing.asset.name}</h1>
+          </Box>
+        </Flex>
+        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: '30%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'red.400',
+                zIndex: -1,
+              }}>
+             {listing.asset.name}
+            </Text>
+            <br />
+            <Text as={'span'} color={'red.400'}>
           <p>
             Owned by <b>{listing.sellerAddress?.slice(0, 6)}</b>
           </p>
-
-          <h2>
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+          <h2 style={{fontSize: 25}}>
             <b>{listing.buyoutCurrencyValuePerToken.displayValue}</b>{" "}
             {listing.buyoutCurrencyValuePerToken.symbol}
           </h2>
-
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 20,
-              alignItems: "center",
-            }}
-          >
-            <button
-              style={{ borderStyle: "none" }}
-              className={styles.mainButton}
-              onClick={buyNft}
-            >
+          </Text>
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={{ base: 'column', sm: 'row' }}>
+            <Button
+			  width={'248px'}
+              rounded={'full'}
+              size={'md'}
+              fontWeight={'normal'}
+              px={6}
+              colorScheme={'red'}
+              bg={'red.400'}
+              _hover={{ bg: 'red.500' }}
+              onClick={buyNft}>
               Buy
-            </button>
-            <p style={{ color: "grey" }}>|</p>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 8,
-              }}
-            >
-              <input
-                type="text"
-                name="bidAmount"
-                className={styles.textInput}
-                onChange={(e) => setBidAmount(e.target.value)}
-                placeholder="Amount"
-                style={{ marginTop: 0, marginLeft: 0, width: 128 }}
-              />
-              <button
-                className={styles.mainButton}
-                onClick={createBidOrOffer}
-                style={{
-                  borderStyle: "none",
-                  background: "transparent",
-                  width: "fit-content",
-                }}
-              >
-                Make Offer
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Container>
   );
 }
