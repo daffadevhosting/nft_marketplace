5b38537e2ac389f50612a503702a71b692bfdeda
daffadevhosting <ndangsaepudin@outlook.com>
2023-01-09 22:12:52 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 }
 
 module.exports = nextConfig
--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@
 
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from './theme'
+import theme from '../const/theme'
 
 export default class Document extends NextDocument {
   render() {
