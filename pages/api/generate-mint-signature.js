09f8ead6ba5384ae8121626bff44cf46a6b3fa0a
daffadevhosting <ndangsaepudin@outlook.com>
2023-01-29 21:38:18 +0700

    update

--- pages/api/generate-mint-signature.js
+++ pages/api/generate-mint-signature.js
@@ -10,7 +10,7 @@ export default async function generateMintSignature(req, res) {
   const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, NETWORK_CHAIN_NAME);
 
   const nftContract = await sdk.getContract(
-    "NFT_COLLECTION_ADDRESS",
+    NFT_COLLECTION_ADDRESS,
     "nft-collection"
   );
 
